54a9b07bbc8d2eb297c52be5fe900fec03bb9087
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-16 15:11:06 +1000

    write triangle tests

--- ShapesTests/TriangleTests.cs
+++ ShapesTests/TriangleTests.cs
@@ -6,10 +6,42 @@ namespace ShapesTests
 {
     public class TriangleTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 1, 1, 1, 1)]
+        [InlineData(3, 5, 7, 2, 87)]
+        [InlineData(3213, 432, 6754, 312, 854327)]
+        [InlineData(543, 4532, 8976, 432, 4312)]
+        public void Constructor(int id, double sideBase, double lengthA, double lengthC, double height)
         {
-            Assert.Equal(1,1);
+            Triangle t = new Triangle(){ID = id, SideBase = sideBase, SideLengthA = lengthA, SideLengthC = lengthC, Height = height};
+
+            Assert.Equal(t.ID, id);
+            Assert.Equal(t.SideBase, sideBase);
+            Assert.Equal(t.SideLengthA, lengthA);
+            Assert.Equal(t.SideLengthC, lengthC);
+            Assert.Equal(t.Height, height);
+        }
+
+        [Theory]
+        [InlineData(12, 13, 78)]
+        [InlineData(3.5, 6.2, 10.85)]
+        [InlineData(10, 32.3, 161.5)]
+        public void Area(double lengthBase, double height, double expected)
+        {
+            Triangle t = new Triangle(){ID = 1, SideBase = lengthBase, Height = height};
+
+            Assert.Equal(expected, t.Area);
+        }
+
+        [Theory]
+        [InlineData(12, 13, 78, 103)]
+        [InlineData(3.5, 6.2, 10.85, 20.55)]
+        [InlineData(10, 32.3, 161.5, 203.8)]
+        public void Circumference(double lengthBase, double sideLengthA, double sideLengthC, double expected)
+        {
+            Triangle t = new Triangle(){ID = 1, SideBase = lengthBase, SideLengthA = sideLengthA, SideLengthC = sideLengthC};
+
+            Assert.Equal(expected, t.Circumference);
         }
     }
 }
