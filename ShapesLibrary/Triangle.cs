38bd5c7c07c73f0983cfbf2e378f859c0dc3ad47
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-16 14:51:55 +1000

    write triangle class

--- ShapesLibrary/Triangle.cs
+++ ShapesLibrary/Triangle.cs
@@ -4,5 +4,18 @@ namespace ShapesLibrary
 {
     public class Triangle
     {
+        //create class
+        //create unit tests
+        //create controller (GET, PUT, POST, DELETE)
+        public int ID { get; set;}
+
+        public double SideBase { get; set; }
+        public double SideLengthA { get; set; }
+        public double SideLengthC { get; set; }
+        public double Height { get; set; }
+
+        public double Circumference => Math.Round(SideBase + SideLengthA + SideLengthC, 2);
+
+        public double Area => Math.Round((SideBase * Height) / 2, 2);
     }
 }
