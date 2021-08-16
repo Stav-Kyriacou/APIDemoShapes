4faaa75f56f321be2e061727b0f3363b4fcc2897
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-16 16:05:24 +1000

    write circle class

--- ShapesLibrary/Circle.cs
+++ ShapesLibrary/Circle.cs
@@ -4,5 +4,11 @@ namespace ShapesLibrary
 {
     public class Circle
     {
+        public int ID { get; set;}
+
+        public double Radius { get; set; }
+        public double Diameter => Radius * 2;
+        public double Area => Math.Round(Math.PI * (Radius * Radius), 2);
+        public double Circumference => Math.Round(2 * Math.PI * Radius, 2);
     }
 }
