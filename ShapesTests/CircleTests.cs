54f3cefc4144b8dc04e0f0310c17ddfd84bffa5d
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-16 16:13:03 +1000

    write circle tests

--- ShapesTests/CircleTests.cs
+++ ShapesTests/CircleTests.cs
@@ -6,10 +6,31 @@ namespace ShapesTests
 {
     public class CircleTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(2, 3214312)]
+        public void Constructor(int id, double radius)
         {
-            Assert.Equal(1,1);
+            Circle c = new Circle(){ID = id, Radius = radius};
+            Assert.Equal(c.ID, id);
+            Assert.Equal(c.Radius, radius);
+        }
+
+        [Theory]
+        [InlineData(2, 12.57)]
+        [InlineData(4, 50.27)]
+        public void Area(double radius, double expected)
+        {
+            Circle c = new Circle(){Radius = radius};
+            Assert.Equal(c.Area, expected);
+        }
+        [Theory]
+        [InlineData(2, 12.57)]
+        [InlineData(4, 25.13)]
+        public void Circumference(double radius, double expected)
+        {
+            Circle c = new Circle(){Radius = radius};
+            Assert.Equal(c.Circumference, expected);
         }
     }
 }
